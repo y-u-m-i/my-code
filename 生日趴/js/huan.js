dfe2cf2118a6301f8ff286ac1202adbfc890ac5b
yumi <120997964@qq.com>
2019-12-24 17:37:59 +0800

    upload

---
+++ 生日趴/js/huan.js
@@ -0,0 +1,114 @@
+
+
+		 function change_pic1(){
+	
+var imgObj = document.getElementById("pic1");
+
+if(imgObj.getAttribute("src",2)=="images/thumbs/1.jpg"){
+imgObj.src="images/1.jpg";
+}
+}
+		 function change_pic2(){
+var imgObj = document.getElementById("pic2");
+if(imgObj.getAttribute("src",2)=="images/thumbs/2.jpg"){
+imgObj.src="images/2.jpg";
+}
+}
+		 function change_pic3(){
+var imgObj = document.getElementById("pic3");
+if(imgObj.getAttribute("src",2)=="images/thumbs/3.jpg"){
+imgObj.src="images/3.jpg";
+}
+}
+		 function change_pic4(){
+var imgObj = document.getElementById("pic4");
+if(imgObj.getAttribute("src",2)=="images/thumbs/4.jpg"){
+imgObj.src="images/4.jpg";
+}
+}
+		 function change_pic5(){
+var imgObj = document.getElementById("pic5");
+if(imgObj.getAttribute("src",2)=="images/thumbs/5.jpg"){
+imgObj.src="images/5.jpg";
+}
+}
+		 function change_pic6(){
+var imgObj = document.getElementById("pic6");
+if(imgObj.getAttribute("src",2)=="images/thumbs/6.jpg"){
+imgObj.src="images/6.jpg";
+}
+}
+
+		 function change_pic7(){
+var imgObj = document.getElementById("pic7");
+if(imgObj.getAttribute("src",2)=="images/thumbs/7.jpg"){
+imgObj.src="images/7.jpg";
+}
+}
+
+		 function change_pic8(){
+var imgObj = document.getElementById("pic8");
+if(imgObj.getAttribute("src",2)=="images/thumbs/8.jpg"){
+imgObj.src="images/8.jpg";
+}
+}
+		 function change_pic9(){
+var imgObj = document.getElementById("pic9");
+if(imgObj.getAttribute("src",2)=="images/thumbs/9.jpg"){
+imgObj.src="images/9.jpg";
+}
+}
+		 function change_pic10(){
+var imgObj = document.getElementById("pic10");
+if(imgObj.getAttribute("src",2)=="images/thumbs/10.jpg"){
+imgObj.src="images/10.jpg";
+}
+}
+		 function change_pic11(){
+var imgObj = document.getElementById("pic11");
+if(imgObj.getAttribute("src",2)=="images/thumbs/11.jpg"){
+imgObj.src="images/11.jpg";
+}
+}
+		 function change_pic12(){
+var imgObj = document.getElementById("pic12");
+if(imgObj.getAttribute("src",2)=="images/thumbs/12.jpg"){
+imgObj.src="images/12.jpg";
+}
+}
+		 function change_pic13(){
+var imgObj = document.getElementById("pic13");
+if(imgObj.getAttribute("src",2)=="images/thumbs/13.jpg"){
+imgObj.src="images/13.jpg";
+}
+}
+		 function change_pic14(){
+var imgObj = document.getElementById("pic14");
+if(imgObj.getAttribute("src",2)=="images/thumbs/14.jpg"){
+imgObj.src="images/14.jpg";
+}
+}
+		 function change_pic15(){
+var imgObj = document.getElementById("pic15");
+if(imgObj.getAttribute("src",2)=="images/thumbs/15.jpg"){
+imgObj.src="images/15.jpg";
+}
+}
+		 function change_pic16(){
+var imgObj = document.getElementById("pic16");
+if(imgObj.getAttribute("src",2)=="images/thumbs/16.jpg"){
+imgObj.src="images/16.jpg";
+}
+}
+		 function change_pic17(){
+var imgObj = document.getElementById("pic17");
+if(imgObj.getAttribute("src",2)=="images/thumbs/17.jpg"){
+imgObj.src="images/17.jpg";
+}
+}
+		 function change_pic18(){
+var imgObj = document.getElementById("pic18");
+if(imgObj.getAttribute("src",2)=="images/thumbs/18.jpg"){
+imgObj.src="images/18.jpg";
+}
+}
